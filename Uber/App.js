5b301e031e3ab156dc02daa70c005932b738ebd8
Mastermind <mastermindx33@gmail.com>
2023-03-08 15:23:30 +0530

    Redux++

---
+++ Uber/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { store } from './store';
+
+// Setup redux - completed
+
+export default function App() {
+  return (
+    <Provider store={store}>
+    <View style={styles.container}>
+      <Text>Lets Build Uber</Text>
+    </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Uber/slices/navSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null,
+}
+
+export const navSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducer: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTimeTravelInformation: (state, action) => {
+            state.timeTravelInformation = action.payload;
+        },
+    }
+});
+
+export const { setOrigin, setDestination, setTimeTravelInformation } = navSlice.actions;
+
+// Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
