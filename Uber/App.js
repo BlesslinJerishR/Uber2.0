2d2c3f0a4ad72365bccb9bd83bbb8b3d077a367c
Mastermind <mastermindx33@gmail.com>
2023-03-08 18:53:23 +0530

    Home Screen Logo

--- Uber/App.js
+++ Uber/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 // Setup redux - completed
@@ -8,9 +9,7 @@ import { store } from './store';
 export default function App() {
   return (
     <Provider store={store}>
-    <View style={styles.container}>
-      <Text>Lets Build Uber</Text>
-    </View>
+    <HomeScreen/>
     </Provider>
   );
 }
---
+++ Uber/screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import { View, Text, SafeAreaView, StyleSheet, Image
+} from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+
+const uberLogo = "https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png";
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+
+        style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+        }}
+
+        source={{
+            uri: uberLogo,
+        }}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+
+})
