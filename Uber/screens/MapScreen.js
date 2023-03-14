8d35d66e69726dab2c0cf7002f8daf39cebb8795
Mastermind <mastermindx33@gmail.com>
2023-03-14 14:13:56 +0530

    Live netlify

--- Uber/screens/MapScreen.js
+++ Uber/screens/MapScreen.js
@@ -4,7 +4,8 @@ import React from 'react'
 const MapScreen = () => {
   return (
     <View>
-      <Text>I am Map</Text>
+      <Text>I am Mapscreen with Google Maps Api & Direction Matrix</Text>
+      <Text>Project Stopped due to Google Api Billing</Text>
     </View>
   )
 }
